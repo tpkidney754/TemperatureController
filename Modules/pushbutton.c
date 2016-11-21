2f0d4b0d97a3d7cb664b8ebec88a22ed9eff36f5
tpkidney754 <tpkidney754@gmail.com>
2016-11-20 19:04:53 -0700

    Added in software delay to debounce the pushbutton signal.

--- Modules/pushbutton.c
+++ Modules/pushbutton.c
@@ -5,8 +5,7 @@ void Button_Init( uint8_t buttonNum )
 {
    SET_BIT_IN_REG( SIM_SCGC5, SIM_SCGC5_PORTA_MASK );
    SET_BIT_IN_REG( BUTTON0, PORT_PCR_IRQC( IRQRising ) |
-                            PORT_PCR_MUX( PIN_GPIO ) |
-                            PORT_PCR_PE_MASK );
+                            PORT_PCR_MUX( PIN_GPIO ) );
    CLEAR_BITS_IN_REG( GPIOC_PDOR, BUTTON0_PIN );
    CLEAR_BITS_IN_REG( GPIOC_PDDR, BUTTON0_PIN );
    NVIC_ClearPendingIRQ( PORTA_IRQn );
@@ -18,6 +17,8 @@ void Button_Init( uint8_t buttonNum )
 void PORTA_IRQHandler( )
 {
    NVIC_DisableIRQ( PORTA_IRQn );
+
+   for( uint32_t i = 0; i < 1000000; i++ );
    if( BUTTON0 & PORT_PCR_ISF_MASK )
    {
       SET_BIT_IN_REG( BUTTON0, PORT_PCR_ISF_MASK );
