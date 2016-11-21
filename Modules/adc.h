d54c6c149c319b14e6c3a7dc18d8803c4802d7c5
tpkidney754 <tpkidney754@gmail.com>
2016-11-20 18:42:08 -0700

    Added in averging to the ADC to stabilize the value being read.

--- Modules/adc.h
+++ Modules/adc.h
@@ -6,6 +6,7 @@
 #define ADC_PORT        SIM_SCGC5_PORTC_MASK
 #define ADC_CH_PIN      PORTC_PCR0
 #define ADC_CHANNEL     AD14
+#define MUX_PIN_ANALOG  0
 
 #define MAX_VALUE       255.0
 #define MAX_DISPLAY_VAL  99.0
@@ -45,6 +46,14 @@ typedef enum
    VREFSL,
    DISABLED,
 } ADC_InputChannel;
+
+typedef enum
+{
+   avg4Samples = 0,
+   avg8Samples,
+   avg16Samples,
+   avg32Samples,
+} ADC_AvgSamples;
 
 void ADC_Init( ADC_InputChannel ADC_ch );
 void ADC_StartConversion( ADC_InputChannel ADC_ch );
